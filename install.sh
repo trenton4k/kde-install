696930c79d9445d7a50a5651b759bae05bf7988e
trenton4k <trenton4k@gmail.com>
2020-09-07 16:04:41 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,3 +1,4 @@
 sudo pacman -S xorg plasma plasma-wayland-session ark dolphin pavucontrol kate kwrite pamac-manager pyakm-manager bleachbit konsole ksysguard
 sudo systemctl enable sddm.service
 sudo systemctl enable NetworkManager.service
+sudo pacman -S  discord feedreader gnome-disk-utility gparted grub-customizer lutris   vlc steam pavucontrol veracrypt teamspeak3 xdg-desktop-portal xdg-desktop-portal-gtk -y
