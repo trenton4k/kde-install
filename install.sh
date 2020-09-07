3b198b58e461fb44c0952cf624c8a993269b6857
trenton4k <trenton4k@gmail.com>
2020-09-07 16:01:13 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,3 @@
+sudo pacman -S xorg plasma plasma-wayland-session ark dolphin pavucontrol kate kwrite pamac-manager pyakm-manager bleachbit konsole ksysguard
+sudo systemctl enable sddm.service
+sudo systemctl enable NetworkManager.service
